1751abc8e3a0d97758dd89eb63c9bc19c9962667
jamhour1g <yazanjam50@gmail.com>
2023-12-20 08:22:57 +0200

    Encapsulate table creation logic in Schema class for better organization
    Improves code structure and  maintainability

--- src/main/java/com/jamhour/database/Database.java
+++ src/main/java/com/jamhour/database/Database.java
@@ -22,104 +22,20 @@ public class Database implements AutoCloseable {
 
     private void setUpSchema() {
         try {
-            String databaseName = "EducationDB";
-            statement.execute("CREATE DATABASE IF NOT EXISTS " + databaseName);
-            statement.execute("USE " + databaseName);
+            statement.execute(Schema.getSchemaCreationQuery());
+            statement.execute(Schema.useSchema());
             createTables();
         } catch (SQLException e) {
-            System.err.println("Failed to create schema. Error: " + e);
+            System.err.println(STR."Failed to create schema. Error: \{e}");
         }
     }
 
     private void createTables() {
-        String createTeacherTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.teacher (
-                    id INT NOT NULL AUTO_INCREMENT,
-                    date_of_birth DATE NOT NULL,
-                    name VARCHAR(50)  NOT NULL,
-                    email VARCHAR(30) NOT NULL,
-                    phone VARCHAR(10) NOT NULL,
-                    salary DOUBLE NOT NULL,
-                    experience INT NOT NULL,
-                    major VARCHAR(30) NOT NULL,
-                    PRIMARY KEY (id)
-                )
-                """;
-
-        String createStudentTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.student (
-                    id INT NOT NULL AUTO_INCREMENT,
-                    name VARCHAR(50) NOT NULL,
-                    email VARCHAR(30) NOT NULL,
-                    phone VARCHAR(10) NOT NULL,
-                    PRIMARY KEY (id)
-                 )
-                """;
-
-        String createCourseTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.course (
-                    id INT NOT NULL,
-                    name VARCHAR(20) NOT NULL,
-                    teacher_id INT DEFAULT NULL,
-                    PRIMARY KEY(id),
-                    KEY FK_TEACHER_ID (teacher_id),
-                    CONSTRAINT FK_TEACHER_ID FOREIGN KEY (teacher_id)
-                    REFERENCES EducationDB.teacher (id) ON DELETE CASCADE
-                )
-                """;
-
-        String createExamTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.exam (
-                 id INT NOT NULL,
-                 course_id INT NOT NULL,
-                 name VARCHAR(50) NOT NULL,
-                 date_time DATETIME NOT NULL,
-                 description TEXT,
-                 PRIMARY KEY (id),
-                 KEY FK_EXAM_COURSE_ID (course_id),
-                 CONSTRAINT FK_EXAM_COURSE_ID FOREIGN KEY (course_id)
-                 REFERENCES EducationDB.course (id) ON DELETE CASCADE
-                 )
-                """;
-
-        String createExamResultsTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.exam_result (
-                exam_id  INT,
-                student_id INT,
-                grade DOUBLE CHECK (grade >= 0 AND grade <= 100),
-                PRIMARY KEY (exam_id,student_id),
-                KEY FK_EXAM_RESULT_ID (exam_id),
-                KEY FK_EXAM_RESULT_STUDENT_ID (student_id),
-                CONSTRAINT FK_EXAM_RESULT_ID FOREIGN KEY (exam_id)
-                REFERENCES exam (id) ON DELETE CASCADE,
-                CONSTRAINT FK_EXAM_RESULT_STUDENT_ID FOREIGN KEY (student_id)
-                REFERENCES student (id) ON DELETE CASCADE
-                )
-                """;
-        String createEnrollmentTable = """
-                CREATE TABLE IF NOT EXISTS EducationDB.enrollment (
-                    student_id INT,
-                    course_id INT,
-                    has_paid BOOLEAN DEFAULT FALSE,
-                    enrollment_status VARCHAR(20)
-                    CHECK (enrollment_status IN ('active', 'completed', 'dropped', 'withdrawn')),
-                    PRIMARY KEY (student_id, course_id),
-                    KEY FK_ENROLLED_STUDENT_ID (student_id),
-                    KEY FK_ENROLLED_COURSE_ID (course_id),
-                    CONSTRAINT FK_STUDENT_ID FOREIGN KEY (student_id)
-                    REFERENCES student (id) ON DELETE CASCADE,
-                    CONSTRAINT FK_COURSE_ID FOREIGN KEY (course_id)
-                    REFERENCES course (id) ON DELETE CASCADE
-                )
-                """;
 
         try {
-            statement.execute(createTeacherTable);
-            statement.execute(createStudentTable);
-            statement.execute(createCourseTable);
-            statement.execute(createExamTable);
-            statement.execute(createExamResultsTable);
-            statement.execute(createEnrollmentTable);
+            for (Schema.Tables table : Schema.Tables.values()) {
+                statement.execute(table.getCreateTableQuery());
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -138,7 +54,7 @@ public class Database implements AutoCloseable {
             connection = dataSource.getConnection();
             statement = connection.createStatement();
         } catch (SQLException e) {
-            System.err.println("Failed to connect to database. Error: " + e);
+            System.err.println(STR."Failed to connect to database. Error: \{e}");
         }
     }
 
@@ -157,6 +73,5 @@ public class Database implements AutoCloseable {
         if (connection != null) {
             connection.close();
         }
-
     }
 }
--- src/main/java/com/jamhour/database/Schema.java
+++ src/main/java/com/jamhour/database/Schema.java
@@ -12,8 +12,14 @@ import java.util.List;
 public enum Schema {
     ;
 
-    public static final String SCHEMA_NAME = "EducationDB";
+    public static final String NAME = "EducationDB";
 
+    static String getSchemaCreationQuery() {
+        return STR."CREATE DATABASE IF NOT EXISTS \{Schema.NAME}";
+    }
+    static String useSchema() {
+        return STR."USE \{Schema.NAME}";
+    }
     @Getter
     public enum Tables {
         TEACHER("teacher"),
@@ -30,6 +36,17 @@ public enum Schema {
             this.tableName = tableName;
             this.columns = getColumns();
 
+        }
+
+        String getCreateTableQuery() {
+            return switch (this) {
+                case TEACHER -> getTeacherTableCreationQuery();
+                case STUDENT -> getStudentTableCreationQuery();
+                case COURSE -> getCourseTableCreationQuery();
+                case EXAM -> getExamTableCreationQuery();
+                case EXAM_RESULT -> getExamResultTableCreationQuery();
+                case ENROLLMENT -> getEnrollmentTableCreationQuery();
+            };
         }
 
         public List<Column> getColumns() {
@@ -77,6 +94,101 @@ public enum Schema {
             };
         }
 
-        public record Column(String name, Class<?> type) {}
+        private static String getEnrollmentTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{ENROLLMENT.getTableName()} (
+                    student_id INT,
+                    course_id INT,
+                    has_paid BOOLEAN DEFAULT FALSE,
+                    enrollment_status VARCHAR(20)
+                    CHECK (enrollment_status IN ('active', 'completed', 'dropped', 'withdrawn')),
+                    PRIMARY KEY (student_id, course_id),
+                    KEY FK_ENROLLED_STUDENT_ID (student_id),
+                    KEY FK_ENROLLED_COURSE_ID (course_id),
+                    CONSTRAINT FK_STUDENT_ID FOREIGN KEY (student_id)
+                    REFERENCES student (id) ON DELETE CASCADE,
+                    CONSTRAINT FK_COURSE_ID FOREIGN KEY (course_id)
+                    REFERENCES course (id) ON DELETE CASCADE
+                )
+                """;
+        }
+
+        private static String getExamResultTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{EXAM_RESULT.getTableName()} (
+                exam_id  INT,
+                student_id INT,
+                grade DOUBLE CHECK (grade >= 0 AND grade <= 100),
+                PRIMARY KEY (exam_id,student_id),
+                KEY FK_EXAM_RESULT_ID (exam_id),
+                KEY FK_EXAM_RESULT_STUDENT_ID (student_id),
+                CONSTRAINT FK_EXAM_RESULT_ID FOREIGN KEY (exam_id)
+                REFERENCES exam (id) ON DELETE CASCADE,
+                CONSTRAINT FK_EXAM_RESULT_STUDENT_ID FOREIGN KEY (student_id)
+                REFERENCES student (id) ON DELETE CASCADE
+                )
+                """;
+        }
+
+        private static String getExamTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{EXAM.getTableName()} (
+                 id INT NOT NULL,
+                 course_id INT NOT NULL,
+                 name VARCHAR(50) NOT NULL,
+                 date_time DATETIME NOT NULL,
+                 description TEXT,
+                 PRIMARY KEY (id),
+                 KEY FK_EXAM_COURSE_ID (course_id),
+                 CONSTRAINT FK_EXAM_COURSE_ID FOREIGN KEY (course_id)
+                 REFERENCES EducationDB.course (id) ON DELETE CASCADE
+                 )
+                """;
+        }
+
+        private static String getCourseTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{COURSE.getTableName()} (
+                    id INT NOT NULL,
+                    name VARCHAR(20) NOT NULL,
+                    teacher_id INT DEFAULT NULL,
+                    PRIMARY KEY(id),
+                    KEY FK_TEACHER_ID (teacher_id),
+                    CONSTRAINT FK_TEACHER_ID FOREIGN KEY (teacher_id)
+                    REFERENCES EducationDB.teacher (id) ON DELETE CASCADE
+                )
+                """;
+        }
+
+        private static String getStudentTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{STUDENT.getTableName()} (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(50) NOT NULL,
+                    email VARCHAR(30) NOT NULL,
+                    phone VARCHAR(10) NOT NULL,
+                    PRIMARY KEY (id)
+                 )
+                """;
+        }
+
+        private static String getTeacherTableCreationQuery() {
+            return STR."""
+                CREATE TABLE IF NOT EXISTS \{NAME}.\{TEACHER.getTableName()} (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    date_of_birth DATE NOT NULL,
+                    name VARCHAR(50)  NOT NULL,
+                    email VARCHAR(30) NOT NULL,
+                    phone VARCHAR(10) NOT NULL,
+                    salary DOUBLE NOT NULL,
+                    experience INT NOT NULL,
+                    major VARCHAR(30) NOT NULL,
+                    PRIMARY KEY (id)
+                )
+                """;
+        }
+
+        public record Column(String name, Class<?> type) {
+        }
     }
 }
