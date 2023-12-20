ff3c6e7f9cb0bda10c2ff5a929a71c90ce1e9395
jamhour1g <yazanjam50@gmail.com>
2023-12-20 20:50:37 +0200

    Enable Course object comparison by implementing Comparable interface

--- src/main/java/com/jamhour/data/Course.java
+++ src/main/java/com/jamhour/data/Course.java
@@ -1,3 +1,19 @@
 package com.jamhour.data;
 
-public record Course(String name, int id,int teacherId) {}
+import java.util.Comparator;
+
+public record Course(String name, int id, int teacherId) implements Comparable<Course> {
+
+
+    private static final Comparator<Course> COMPARATOR =
+            Comparator
+                    .comparingInt(Course::id)
+                    .thenComparing(Course::teacherId)
+                    .thenComparing(Course::name);
+
+
+    @Override
+    public int compareTo(Course other) {
+        return COMPARATOR.compare(this, other);
+    }
+}
