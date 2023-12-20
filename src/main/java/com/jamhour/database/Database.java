149d8f9832ac70a63cf01d3613d87ba390a4a013
jamhour1g <yazanjam50@gmail.com>
2023-12-20 14:51:18 +0200

    Encapsulate database access within its package for controlled visibility
    Restricts external access to database class, promoting modularity and data protection

--- src/main/java/com/jamhour/database/Database.java
+++ src/main/java/com/jamhour/database/Database.java
@@ -1,18 +1,22 @@
 package com.jamhour.database;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import lombok.Getter;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Database implements AutoCloseable {
+@Getter
+class Database implements AutoCloseable {
     private static Database instance;
 
     private final MysqlDataSource dataSource = new MysqlDataSource();
 
     private Connection connection;
     private Statement statement;
+
+    private boolean connected;
 
     private Database() {
         setUpDataSource();
@@ -53,6 +57,7 @@ public class Database implements AutoCloseable {
         try {
             connection = dataSource.getConnection();
             statement = connection.createStatement();
+            connected = true;
         } catch (SQLException e) {
             System.err.println(STR."Failed to connect to database. Error: \{e}");
         }
